cf9d56b3054a3e05b8ab2d93cae1f6303a2b0032
Moritz Schmidt <fusselwurm@posteo.de>
2017-10-01 11:12:23 +0200

    Update arma3-exec-restarts.php

--- arma3-exec-restarts.php
+++ arma3-exec-restarts.php
@@ -7,36 +7,37 @@ const USER = 'arma3server';
 
 
 if (get_current_user() !== USER) {
-	echo "run me as " . USER;
-	exit(1);
+        echo "run me as " . USER;
+        exit(1);
 }
 
 chdir(ARMA3_PATH);
 
 while (true) {
-	sleep(SLEEP);
-	if (!file_exists(RESTART_TRIGGER_FILENAME)) {
-		continue;
-	}
+        sleep(SLEEP);
+        if (!file_exists(RESTART_TRIGGER_FILENAME)) {
+                continue;
+        }
 
-	$contents = file_get_contents(RESTART_TRIGGER_FILENAME);
-	file_put_contents(RESTART_TRIGGER_FILENAME, '');
+        $contents = file_get_contents(RESTART_TRIGGER_FILENAME);
+        file_put_contents(RESTART_TRIGGER_FILENAME, '');
 
-	$lines = array_filter(explode("\n", $contents));
-	$commands = array_filter(array_map('json_decode', $lines));
+        $lines = array_filter(explode("\n", $contents));
+        $commands = array_filter(array_map('json_decode', $lines));
 
-	$a3ups = [];
-	foreach ($commands as $command) {
-		$port = intval(isset($command->port) ? $command->port : 0);
-		$template = isset($command->template) ? trim($command->template) : '';
-		if (($port === 0) || ($template && (strpos('.', $template) !== false))) {
-			syslog(LOG_ALERT, 'invalid parameters from arma3server-webrestart: ' . json_encode($command));
-			break;
-		}
-		$a3ups[$port] = sprintf("./a3up.sh %s %s & ", $port, $template);
-	}
-	foreach ($a3ups as $a3up) {
-		echo sprintf("executing %s in %s…\n", $a3up, getcwd());
-		shell_exec($a3up);
-	}
+        $a3ups = [];
+        foreach ($commands as $command) {
+                $port = intval(isset($command->port) ? $command->port : 0);
+                $template = isset($command->template) ? trim($command->template) : '';
+                if (($port === 0) || ($template && (strpos('/', $template) !== false))) {
+                        syslog(LOG_ALERT, 'invalid parameters from arma3server-webrestart: ' . json_encode($command));
+                        break;
+                }
+                $a3ups[$port] = sprintf("./a3up.sh %s %s & ", $port, $template);
+        }
+        foreach ($a3ups as $a3up) {
+                echo sprintf("executing %s in %s…\n", $a3up, getcwd());
+                $out = shell_exec($a3up);
+                echo "\n" . str_replace("\n", "\n\t", $out) . "\n";
+        }
 }
